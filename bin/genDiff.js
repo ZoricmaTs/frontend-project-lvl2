7f7eec1acaf299ef94bab7fb8c8a1a21fa52bcf0
ZoricmaTs <tszzzch@gmail.com>
2021-07-10 22:52:41 +0700

    fix

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -5,7 +5,7 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 program
-  // .version('0.0.1')
+  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-V, --version', 'output the version number')
