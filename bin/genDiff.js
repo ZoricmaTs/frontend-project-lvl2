0f0da1908c01a6e023c7790d5f555e55e00392bd
ZoricmaTs <tszzzch@gmail.com>
2021-07-10 14:58:20 +0700

    added options

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { commander } from 'commander';
-const program = new commander();
+import { program } from 'commander';
 
 program
-  .version('0.1.0')
+  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
