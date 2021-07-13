d39c7564a34ee31c5ab169d8593962617c686846
ZoricmaTs <tszzzch@gmail.com>
2021-07-14 01:24:52 +0700

    added diff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const makeDiff = (filepath1, filepath2) => {
-  return console.log('filepath1', filepath1, filepath2);
+  const fileObj1 = JSON.parse(filepath1);
+  const fileObj2 = JSON.parse(filepath2);
+  const allKeys = [...Object.keys(fileObj1), ...Object.keys(fileObj2)];
+  const keys = allKeys.filter((item, index) => allKeys.indexOf(item) === index);
+  const keysSorted = keys.sort();
+  const diff = keysSorted.reduce((acc, key) => {
+    if (!fileObj2.hasOwnProperty(key)) {
+      return [ ...acc, `- ${key}: ${fileObj1[key]}`]
+    }
+    if (!fileObj1.hasOwnProperty(key)) {
+      return [ ...acc, `+ ${key}: ${fileObj2[key]}`]
+    }
+    if (fileObj1[key] !== fileObj2[key]) {
+      return [ ...acc, `- ${key}: ${fileObj1[key]}`, `+ ${key}: ${fileObj2[key]}`]
+    }
+
+    return [ ...acc, `  ${key}: ${fileObj1[key]}`]
+  }, []);
+
+  let str = '{\n';
+  diff.forEach((item) => {
+    str += `  ${item}\n`;
+  });
+
+  return `${str}}`
 };
 
 export default makeDiff;
